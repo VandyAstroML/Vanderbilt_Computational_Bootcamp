f5563f1431cc5e192ba689cc1e3951b8aaaff20a
VICTOR CALDERON <vcalderon2009@my.fit.edu>
2017-02-13 15:12:38 -0600

    Added bash script for copying notebooks into folder in order to make notes

--- nb_copy.sh
+++ nb_copy.sh
@@ -3,18 +3,35 @@
 #
 # Description:  Copies the notebook for a given week.
 # 
-# How to run:    ./nb_copy.sh week_num
+# How to run:    ./nb_copy.sh week_num ow_opt
 #
 # Parameters
 # ----------
-# week_num: number of the week of the iPython notebook to copy
+# - week_num: number of the week of the iPython notebook to copy
+# - ow_opt: if "y", it will overwrite the notebooks in `nb_copy_path'
+
+# Constants
+nb_path='./notebooks'
+nb_copy_path='./notebooks_notes'
 
 # Parameters
 week_num=$1
-nb_path='./notebooks'
-nb_copy_path='./notebooks_notes'
-# Run commands
-# git pull;
+ow_opt=$2
 
-echo "cp -rp ${np_path}/Week_${week_num}/ ${nb_copy_path}"
-cp -rp ${np_path}/Week_${week_num}/ ${nb_copy_path}
+# Reading in Parameters
+if  [ "$1" == "-h" ] || \
+    [ "$#" -eq "0"  ]; then
+    echo "How to run:    ./nb_copy.sh week_num overwrite_opt"
+    echo "  * week_num: number of the week of the iPython notebook to copy"
+    echo "  * ow_opt: if 'true', it will overwrite the notebooks in 'nb_copy_path'"
+else
+    echo "git pull"
+    git pull
+    if [ "$#" -ge "1" ] && \
+       [ "$ow_opt" == "y" ]; then
+        echo "cp  -rp ${nb_path}/Week_${week_num} ${nb_copy_path}/"
+        cp  -rp ${nb_path}/Week_${week_num} ${nb_copy_path}/
+    fi
+    echo "jupyter notebook ${nb_copy_path}/Week_${week_num}/*.ipynb"
+    jupyter notebook ${nb_copy_path}/Week_${week_num}/*.ipynb
+fi
